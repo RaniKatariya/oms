c2b814d76315991b540bce465415e7fbd2f0ccdf
RaniKatariya <Katariyarani68@gmail.com>
2017-10-02 23:38:46 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace OMS.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int eid { get; set; }
+        public string  ename { get; set; }
+        public string  dept { get; set; }
+        public string  address { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
